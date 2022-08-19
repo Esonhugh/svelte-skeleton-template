9cbf1a8f0cb370235061c697761bf25db5fd7797
Esonhugh <esonhughoutside@gmail.com>
2022-08-20 00:24:34 +0800

    init all.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-light text-3xl text-center">
+    Welcome to SvelteKit
+</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
